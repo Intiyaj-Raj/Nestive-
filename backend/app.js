1d6bcf58fd30882e5b2206bab55ab7fbd5b11742
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-21 20:31:27 +0530

    perform crud operation

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/nestive";
 
 main()
   .then(() => {
@@ -26,6 +26,68 @@ app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hii, I am root.");
+});
+
+// index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listing/index.ejs", { allListings });
+});
+
+// new route
+app.get("/listings/new", (req, res) => {
+  res.render("listing/new.ejs");
+});
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listing/show.ejs", { listing });
+});
+
+// create route
+app.post("/listings", async (req, res) => {
+  try {
+    let { title, description, image, price, country, location } = req.body;
+
+    const newListing = new Listing({
+      title,
+      description,
+      image,
+      price,
+      country,
+      location,
+    });
+
+    await newListing.save();
+
+    res.redirect("/listings");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listing/edit.ejs", { listing });
+});
+
+// update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body });
+  res.redirect(`/listings/${id}`);
+});
+
+// delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deltetedListing = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 app.listen(3000, () => {
   console.log("server is running on 3000 port");
