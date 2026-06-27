5cb04ebeacc7a91195eddd379f6215e49327a363
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-28 01:14:22 +0530

    error handling

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-
+const ExpressError = require("./utils/ExpressError.js")
 const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/nestive";
+const { listingSchema } = require("./schema.js");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 main()
   .then(() => {
@@ -29,6 +31,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.engine("ejs", ejsMate);
 
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body)
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",")
+    throw new ExpressError(400, errMsg)
+  }
+  else {
+    next()
+  }
+}
 app.get("/", (req, res) => {
   res.send("Hii, I am root.");
 });
@@ -54,27 +67,20 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 // create route
-app.post("/listings", async (req, res) => {
-  try {
-    let { title, description, image, price, country, location } = req.body;
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
+  const { title, description, image, price, country, location } = req.body.listing;
 
-    const newListing = new Listing({
-      title,
-      description,
-      image,
-      price,
-      country,
-      location,
-    });
-
-    await newListing.save();
-
-    res.redirect("/listings");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
-  }
-});
+  const newListing = new Listing({
+    title,
+    description,
+    image,
+    price,
+    country,
+    location,
+  });
+  await newListing.save();
+  res.redirect("/listings");
+}));
 
 // edit route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -84,11 +90,12 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 // update route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body });
-  res.redirect(`/listings`);
-});
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+})
+);
 
 // delete route
 app.delete("/listings/:id", async (req, res) => {
@@ -104,6 +111,19 @@ app.get("/privacy", (req, res) => {
 
 app.get("/terms", (req, res) => {
   res.render("pages/terms");
+});
+
+app.all("/*splat", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+  if (err.name === "CastError") {
+    err = new ExpressError(404, "Listing not found!");
+  }
+
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("listing/error.ejs", { message });
 });
 app.listen(3000, () => {
   console.log("server is running on 3000 port");
