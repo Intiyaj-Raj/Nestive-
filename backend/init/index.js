51bc180b4dac42452bdacebb9f7c39be05c58f9b
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-07-20 00:07:51 +0530

    add authentication

--- backend/init/index.js
+++ backend/init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: "6a5a7d8f03bc68ecd8f5de4f" }))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
